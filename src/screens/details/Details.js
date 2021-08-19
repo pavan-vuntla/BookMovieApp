270d446d5775696ec59dbc9a62af7e3028255dc6
Pavan Vuntla <pavan.vuntla@gmail.com>
2021-08-19 17:22:09 +0530

    made changes to show only 6 tiles in the upcoming movies section

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,130 +1,130 @@
-import React from 'react';
-import Header from "../../common/header/Header";
-import Typography from "@material-ui/core/Typography";
-import "./Details.css"
-import { withStyles } from "@material-ui/core/styles";
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import GridListTileBar from "@material-ui/core/GridListTileBar";
-import YouTube from "react-youtube";
-import StarRating from "../../common/rating/Rating";
+  import React from 'react';
+  import Header from "../../common/header/Header";
+  import Typography from "@material-ui/core/Typography";
+  import "./Details.css"
+  import { withStyles } from "@material-ui/core/styles";
+  import GridList from "@material-ui/core/GridList";
+  import GridListTile from "@material-ui/core/GridListTile";
+  import GridListTileBar from "@material-ui/core/GridListTileBar";
+  import YouTube from "react-youtube";
+  import StarRating from "../../common/rating/Rating";
 
 
-const styles = () => ({
-  emptyStar: {
-    color: "black",
-  },
-  marginTop16:{
-      marginTop:"16px"
-  },
-  artistMargin:{
-       marginTop:"16px",
-      marginBottom:"16px"
-  }
-});
-function Details(props) {
-    
-    const regex =  new RegExp(/(?<==)(\w*)/gm);
-    
-    let videoId = props.location.state.trailer_url.match(regex);
-    console.log("details",regex);
-    console.log("details",videoId);
-  const backHomeHandler = () => {
-
-    props.history.push("/")
-  };
+  const styles = () => ({
+    emptyStar: {
+      color: "black",
+    },
+    marginTop16:{
+        marginTop:"16px"
+    },
+    artistMargin:{
+        marginTop:"16px",
+        marginBottom:"16px"
+    }
+  });
+  function Details(props) {
+      
+      const regex =  new RegExp(/(?<==)(\w*)/gm);
+      
+      let videoId = props.location.state.trailer_url.match(regex);
+      console.log("details",regex);
+      console.log("details",videoId);
+      const backHomeHandler = () => {
+          props.history.push("/")
+          };
 
   const { classes } = props;
 
   const opts = {
-    
-    playerVars: {
-      autoplay: 0,
       
-    },
-  };
+      playerVars: {
+        autoplay: 0,
+        'origin': 'http://localhost:3000',
+        
+      },
+    };
 
-    const VideoOnReady=(e)=> {
-    e.target.pauseVideo();
-    console.log(e.target);
-  };
+  const VideoOnReady=(e)=> {
+      e.target.pauseVideo();
+      console.log("Video",e.target);
+    };
 
 
 
-  return (
-    <div>
-      <Header hideBookShow={false} />
-      <div className="back" onClick={backHomeHandler}>
-        {`< Back to Home`}
+    return (
+      <div>
+        <Header hideBookShow={false} />
+        <div className="back" onClick={backHomeHandler}>
+          {`< Back to Home`}
+        </div>
+        <div className="details">
+          <div className="left" >
+            <img
+              src={props.location.state.poster_url}
+              alt={props.location.state.title}
+              className="imgwidth"
+            />
+          </div>
+          <div className="middle">
+            <Typography variant="headline" component="h2">
+              {props.location.state.title}
+            </Typography>
+            <Typography>
+              <strong>Genre: </strong>
+              {props.location.state.genres.join(",")}
+            </Typography>
+            <Typography>
+              <strong>Duration: </strong>
+              {props.location.state.duration}
+            </Typography>
+            <Typography>
+              <strong>Release Date: </strong>
+              {new Date(props.location.state.release_date).toDateString()}
+            </Typography>
+            <Typography>
+              <strong>Rating: </strong>
+              {props.location.state.rating}
+            </Typography>
+            <Typography className={classes.marginTop16}>
+              <strong>Plot: </strong>(
+              <a href={props.location.state.wiki_url}>Wiki Link</a>)
+              {props.location.state.storyline}
+            </Typography>
+            <Typography className={classes.marginTop16}>
+              <strong>Trailer: </strong>
+            </Typography>
+            <YouTube videoId={videoId[0]} opts={opts} host= 'http://www.youtube.com' onReady={VideoOnReady} />;
+          </div>
+          <div className="right">
+            <Typography>
+              <strong>Rate this movie:</strong>
+            </Typography>
+
+            <Typography component={'span'} style={{width:"120px"}}>
+              <StarRating />
+            </Typography>
+            <Typography className={classes.artistMargin}>
+              <strong>Artists:</strong>
+            </Typography>
+            {props.location.state.artists?
+            <GridList cols={2}>
+              {props.location.state.artists.map((artist) => (
+                <GridListTile key={artist.id} style={{ padding: "10px" }}>
+                  <img
+                    margin="auto"
+                    src={artist.profile_url}
+                    alt={artist.first_name}
+                  />
+                  <GridListTileBar
+                    title={`${artist.first_name} ${artist.last_name}`}
+                  />
+                </GridListTile>
+              ))}
+            </GridList>:null}
+          </div>
+        </div>
       </div>
-      <div className="details">
-        <div className="left" >
-          <img
-            src={props.location.state.poster_url}
-            alt={props.location.state.title}
-            className="imgwidth"
-          />
-        </div>
-        <div className="middle">
-          <Typography variant="headline" component="h2">
-            {props.location.state.title}
-          </Typography>
-          <Typography>
-            <strong>Genre: </strong>
-            {props.location.state.genres.join(",")}
-          </Typography>
-          <Typography>
-            <strong>Duration: </strong>
-            {props.location.state.duration}
-          </Typography>
-          <Typography>
-            <strong>Release Date: </strong>
-            {new Date(props.location.state.release_date).toDateString()}
-          </Typography>
-          <Typography>
-            <strong>Rating: </strong>
-            {props.location.state.rating}
-          </Typography>
-          <Typography className={classes.marginTop16}>
-            <strong>Plot: </strong>(
-            <a href={props.location.state.wiki_url}>Wiki Link</a>)
-            {props.location.state.storyline}
-          </Typography>
-          <Typography className={classes.marginTop16}>
-            <strong>Trailer: </strong>
-          </Typography>
-          <YouTube videoId={videoId[0]} opts={opts} host= 'https://www.youtube.com' onReady={VideoOnReady} />;
-        </div>
-        <div className="right">
-          <Typography>
-            <strong>Rate this movie:</strong>
-          </Typography>
+    );
+  }
 
-          <Typography component={'span'} style={{width:"120px"}}>
-            <StarRating />
-          </Typography>
-          <Typography className={classes.artistMargin}>
-            <strong>Artists:</strong>
-          </Typography>
-
-          <GridList cols={2}>
-            {props.location.state.artists?props.location.state.artists.map((artist) => (
-              <GridListTile key={artist.id} style={{ padding: "10px" }}>
-                <img
-                  margin="auto"
-                  src={artist.profile_url}
-                  alt={artist.first_name}
-                />
-                <GridListTileBar
-                  title={`${artist.first_name} ${artist.last_name}`}
-                />
-              </GridListTile>
-            )):null}
-          </GridList>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default withStyles(styles)(Details);
+  export default withStyles(styles)(Details);
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,16 +1,26 @@
 import React,{useEffect, useState} from "react";
 import Header from "../../common/header/Header";
+import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import GridList from "@material-ui/core/GridList";
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import "./Home.css";
 import Filters from './Filters'
-import moment from 'moment'
 
+const styles = () => ({
+   root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    },
+    gridList: {
+    flexWrap: 'nowrap',
+    transform: 'translateZ(0)',
+  },
 
-
-
+  });
 const Home = (props) => {
     let data = [];
     const[upcomingPoster,setUpcomingPoster]=useState([]);
@@ -30,41 +40,32 @@ const Home = (props) => {
    
    },[]);
 
-             async function fetchFilterData(filterURL) {
-               console.log(filterURL);
-             const response = await fetch(
-               filterURL
-             );
-             const json = await response.json();
-             data = await json.movies;
-             setReleasedPoster(data); 
+  async function fetchFilterData(filterURL) {
+          console.log(filterURL);
+          const response = await fetch(
+          filterURL
+          );
+          const json = await response.json();
+          data = await json.movies;
+          setReleasedPoster(data); 
           }
+   const { classes } = props;      
 
    const movieDetailsHandler=(e,details)=>{
      console.log("details",details);
      props.history.push("/movie/" + details.id,
       details);
-
-   }
+      }
    
   return (
     <div>
-      <Header hideBookShow={true} {...props} />
+      <Header hideBookShow={true} baseUrl={props.baseUrl}/>
       <h3>Upcoming Movies</h3>
-
-      <GridList cols={6} cellHeight={250} >
-        <Grid
-          item
-
-          style={{ display: "flex", flexDirection: "row" }}
-          
-        >
+      <div className={classes.root}>
+      <GridList className={classes.gridList} cols={6} cellHeight={250} >
           {upcomingPoster.map((movie) => (
             <GridListTile
               key={movie.id}
-              style={{
-                height: "250px",
-              }}
               onClick={(e) => movieDetailsHandler(e, movie)}
             >
               <img
@@ -76,8 +77,8 @@ const Home = (props) => {
               <GridListTileBar title={movie.title} />
             </GridListTile>
           ))}
-        </Grid>
       </GridList>
+      </div>
       <div style={{ width: "100%", display: "flex" }}>
         <div style={{ width: "76%", margin: "16px" }}>
           <GridList cellHeight={350} cols={4}>
@@ -95,9 +96,7 @@ const Home = (props) => {
                     subtitle={
                       <span>
                         Release Date:
-                        {moment(new Date(movie.release_date)).format(
-                          "ddd MMM D Y"
-                        )}
+                        {new Date(movie.release_date).toDateString()}
                       </span>
                     }
                   />
@@ -113,4 +112,4 @@ const Home = (props) => {
   );
 };
 
-export default Home;
+export default withStyles(styles)(Home);
